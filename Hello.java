00e86d451c66221a7808ad8b8cc1b42af365cd2d
speed star <ehgur1104@naver.com>
2018-10-28 22:02:16 +0900

    Second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
  public class Hello {
     public static void main(String[] args) {
           System.out.println("Hello, world from GIT!");
-             }
+          Sustem.out.println("Changes after First comit"); 
+  }
                }
